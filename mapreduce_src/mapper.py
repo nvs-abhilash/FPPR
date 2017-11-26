4b84cbced83c5497d60376fd10f18e2544dee67d
nvs-abhilash <nvs232@gmail.com>
2017-11-26 17:42:30 +0530

    mapper: Add mapper

---
+++ mapreduce_src/mapper.py
@@ -0,0 +1,32 @@
+"""
+Given u, v -> (node, adjacency list)
+"""
+import sys
+
+def main():
+    graph_dict = {}
+
+    for line in sys.stdin:
+        u, v, w, t = line.strip().split(",")
+
+        try:
+            graph_dict[u].append(v)
+        except:
+            graph_dict[u] = [v]
+
+    for key in graph_dict.keys():
+        sys.stdout.write(key)
+        if len(graph_dict[key]) > 0:
+            sys.stdout.write(',')
+
+        cnt = 0
+        for val in graph_dict[key]:
+            sys.stdout.write(val)
+            cnt += 1
+            if cnt != len(graph_dict[key]):
+                sys.stdout.write(',')
+
+        sys.stdout.write("\n")
+
+if __name__ == '__main__':
+    main()
